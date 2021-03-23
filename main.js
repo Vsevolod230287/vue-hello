a2983f01ab39915efe75584236e935c54804b6b2
Vsevolod <cerneavschivsevolod@gmail.com>
2021-03-23 20:34:42 +0100

    Fatto ! anche il bonus!

---
+++ main.js
@@ -0,0 +1,11 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+var app = new Vue({
+  el: '#root',
+  data: {
+    message: 'Ciao Mondo!',
+    image: 'https://blog.matcharesident.com/wp-content/uploads/2019/07/iStock-944453634-750x450.jpg'
+  }
+})
